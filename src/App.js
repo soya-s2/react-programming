e213b656ffa612ca7846093711684884c789dc93
soya-s2 <soya.s2.dev@gmail.com>
2021-08-15 00:49:15 +0900

    Use inline styling 2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 function App() {
   const name = '리액트';
-  const style = {
-    // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성함.
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16 // 단위를 생략하면 px로 지정됨.
-  };
-  return <div style={style}>{name}</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16
+      }}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default App;
 
-// style 객체를 미리 선언하고 div의 style 값으로 지정한 것이다.
+// style 객체를 미리 선언하지 않고 바로 style 값을 지정한 것이다.
