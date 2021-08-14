db07bed078d8bc309994aa7556bc983f02323099
soya-s2 <soya.s2.dev@gmail.com>
2021-08-15 00:53:14 +0900

    not class, but className

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   const name = '리액트';
-  return (
-    <div
-      style={{
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16
-      }}
-    >
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>
 }
 
 export default App;
-
-// style 객체를 미리 선언하지 않고 바로 style 값을 지정한 것이다.
