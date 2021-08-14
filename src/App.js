e73899e0459228115e5be2eceeab05222ca9ae13
soya-s2 <soya.s2.dev@gmail.com>
2021-08-14 18:59:23 +0900

    Use variable in JSX

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
     <>
-      <h1>리액트 안녕!</h1>
+      <h1> {name} 안녕!</h1>
       <h1>동작 중!</h1>
     </>
   );
