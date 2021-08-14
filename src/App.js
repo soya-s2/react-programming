409a67ac96afb70ef0d601f4bfac9a50830cede3
soya-s2 <soya.s2.dev@gmail.com>
2021-08-15 00:58:43 +0900

    Use self-closing tag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return <div className="react">{name}</div>
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input /> 
+    </>
+  );
 }
 
 export default App;
+
+/* 
+line 9의 <input />과 같은 것을 self-closing tag라고 하며,
+태그를 선언하면서 동시에 닫을 수 있다.
+또한 JSX 문법에서는 input, br 등을 꼭 닫아야 한다.
+*/
