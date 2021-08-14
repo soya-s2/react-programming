b13aa389a9edbaffd80e3f6c81c7144e0e8e4e37
soya-s2 <soya.s2.dev@gmail.com>
2021-08-15 01:06:47 +0900

    How to use 'ESLint' and 'Prettier'

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import './App.css';
 function App() {
   const name = '리액트';
   return (
-    <>
-      {/* 주석은 이렇게 작성합니다. */}
-      <div
-        className="react" // 시작 태그를 여러 줄로 작성하면 여기에 주석이 작성 가능하다.
-      >
-        {name}
-      </div>
-      // 하지만 이런 주석이나
-      /* 이런 주석은 페이지에 그대로 나타나게 된다. */
-      <input />
-    </>
+    <div>
+      <div className="react">{name}</div>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </div>
   );
 }
 
