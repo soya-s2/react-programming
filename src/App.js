ffd32050e2867a4bec93a77d92ab135eeae374ee
soya-s2 <soya.s2.dev@gmail.com>
2021-08-15 00:47:16 +0900

    Use inline styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  const name = undefined;
-  return name;
-  // return name || '값이 undefined입니다.'; -> 이렇게 하면 방지 가능.
+  const name = '리액트';
+  const style = {
+    // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성함.
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 // 단위를 생략하면 px로 지정됨.
+  };
+  return <div style={style}>{name}</div>;
 }
 
 export default App;
 
-/*
-Error: App(...): Nothing was returned from render. 라는 오류가 발생한다.
-리액트 컴포넌트에서는 함수에서 undefined만 반환하여 렌더링하는 상황을 만들면 안 된다.
-
-또한 JSX 내부에서 undefined를 return <div>{name}</div>처럼 렌더링하는 것은 가능하다.
-
-undefined일 때 보여주고 싶은 문구가 있다면
-return <div>{name || '리액트'}</div>와 같이 쓰면 된다.
-*/
+// style 객체를 미리 선언하고 div의 style 값으로 지정한 것이다.
