426825651fd55f0b9fb77c90bf40c92aa76e4fc0
soya-s2 <soya.s2.dev@gmail.com>
2021-08-14 19:17:55 +0900

    How will undefined value be rendered?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-function App() {
-  const name = '리액트';
-  // const number = 0;
-  return (
-    <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>
-  );
-  // return number && <div>내용</div>; -> 이것은 화면에 0으로 나타난다.
-}
+import React from 'react';
+import './App.css';
 
-/*
 function App() {
-  const name = '뤼왝트';
-  return (
-    <div>{name === '리액트' ? (<h1>리액트입니다.</h1>) : null}</div>
-  );
+  const name = undefined;
+  return name;
 }
-
-위와 같이 코드를 작성하고 나면, '뤼액트'일 때는 아무 것도 보이지 않으며
-'리액트'일 때는 렌더링한다.
-return 값이 false를 렌더링할 때는 아무것도 나타나지 않는 원리이다.
-※ falsy한 값인 0은 예외적으로 화면에 나타난다.
-*/
 
 export default App;
+
+/*
+Error: App(...): Nothing was returned from render. 라는 오류가 발생한다.
+리액트 컴포넌트에서는 함수에서 undefined만 반환하여 렌더링하는 상황을 만들면 안 된다.
+*/
