2834d4ee59c3765e51c132e0537057fb50d0c2c5
soya-s2 <soya.s2.dev@gmail.com>
2021-08-16 19:12:12 +0900

    Use destructuring assignment

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const MyComponent = (props) => {
+  const { name, children } = props; // 우항의 키 값이 좌항의 변수명과 매칭된다.
   return (
     <div>
-      안녕하세요, 제 이름은 {props.name}입니다. <br />
-      children 값은 {props.children}입니다.
+      안녕하세요, 제 이름은 {name}입니다. <br />
+      children 값은 {children}입니다.
     </div>
   );
 };
