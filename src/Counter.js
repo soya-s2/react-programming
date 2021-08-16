8d5fba1bd3a4d3809e4c08f413ab010148c6da89
soya-s2 <soya.s2.dev@gmail.com>
2021-08-16 19:54:29 +0900

    Use several value of state

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
-    const { number } = this.state; // state를 조회할 때는 this.state로 조회한다.
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다.
           onClick={() => {
             // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다.
             this.setState({ number: number + 1 });
+            this.setState({ number: this.state.number + 1 });
+            // 비동기적으로 상태를 업데이트 하기 때문에 number값이 바로 바뀌지 않는다.
+            // 이를 해결하기 위한 방법으로는 this.setState에 인자를 함수로 넣어주는 것이 있다.
+
+            /*
+            this.setState((prevState, props) => {
+              return {
+                // 업데이트 하고 싶은 내용
+              }
+            }) 
+            */
           }}
         >
           +1
