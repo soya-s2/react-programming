024fa5452efd2b8b0a04278bd012d9b523286383
soya-s2 <soya.s2.dev@gmail.com>
2021-09-23 23:06:02 +0900

    Input object to useState function

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
 
-import React from 'react';
-
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
-  const onClick = () => {
+  // useState를 통한 state에 문자열이 아닌 객체를 넣어본다.
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form, // 기존 form 내용을 여기에 복사한다.
+      [e.target.name]: e.target.value // 원하는 값을 덮어 씌운다. 
+      // 즉 기존의 초깃값을 유지하면서 두 input중 내용에 변화가 있는 이벤트 대상만 변경된다.
+    };
+    setForm(nextForm); // form 값을 덮어 씌운다.
+  }
+  const onClick = () => { 
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    })
   }
   const onKeyPress = e => {
     if(e.key === 'Enter') {
@@ -26,14 +37,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username} // value에 message를 설정
-        onChange={onChangeUsername}
+        onChange={onChange}
         />
         <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요."
         value={message} // value에 message를 설정
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
         />
         <button onClick={onClick}>확인</button>
