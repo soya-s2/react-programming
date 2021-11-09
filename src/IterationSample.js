57ecf4fba8c1556fe64b0e531b02dc8a37d6d356
soya-s2 <soya.s2.dev@gmail.com>
2021-11-09 18:25:53 +0900

    Configure key in component list

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 const IterationSample = () => {
   const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map(name => <li>{name}</li>); // 데이터 배열을 컴포넌트 배열로 변환하는 부분
-  /*
-  function (name) {
-    return <li>{name}</li>
-  }
-  */
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
   return (
     <ul>
       {nameList}
@@ -18,13 +13,10 @@ const IterationSample = () => {
 export default IterationSample;
 
 /* 
-위의 코드를 실행 시 콘솔에서 
-Warning: Each child in a list should have a unique "key" prop.
-오류를 만날 수 있다.
+더이상 key 에러를 표시하지 않는다.
+컴포넌트 배열 각각에 key가 설정되었기 때문이다.
+key 설정은 컴포넌트의 props를 설정하는 것과 같은 방식으로 하면 된다.
 
-리액트에서 key는 컴포넌트 배열 렌더링 시
-어떤 원소에 변동이 있었는지 알아내기 위해 쓴다.
-유동적인 데이터를 다룰 때, 원소를 생성, 제거, 수정할 수도 있기 때문.
-key가 없다면 Virtual DOM을 비교하는 과정에서 리스트를 순차 비교하지만 
-key가 있다면 이 값을 사용해서 어떤 변화가 일어났는지 더 빠르게 알 수 있다.
+※ 적절한 고유 값이 없을 때만 index를 key로 설정한다.
+index를 key로 사용하면 배열 변경 시 효율적인 리렌더링이 불가하기 때문이다.
 */
