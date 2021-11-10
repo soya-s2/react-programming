450c28011dab1e7c7b1cd2354844e926d246abb8
soya-s2 <soya.s2.dev@gmail.com>
2021-11-10 15:37:45 +0900

    Use useReducer / different element, same event - { [e.target.name]: [e.target.value] }

--- src/Info.js
+++ src/Info.js
@@ -1,43 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
 
-/* ------------------------------------------------------------------------
-  3. 컴포넌트가 언마운트되기 전이나 업데이트되기 직전에 수행하고 싶은 작업이 있다면
-  useEffect에서 뒷정리(cleanup) 함수를 반환해야 한다.
-  컴포넌트가 나타날 때 effect, 사라질 때 cleanup이 나타난다.
-
-  렌더링될 때마다 뒷정리 함수가 계속 나타나며, 
-  뒷정리 함수가 호출될 때는 업데이트되기 직전의 값을 보여준다.
-  언마운트될 때만 뒷정리 함수를 호출하고 싶다면 
-  useEffect 함수의 두 번째 파라미터에 비어 있는 배열을 넣는다.
----------------------------------------------------------------------------*/
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, [name]);
-
-
-
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
