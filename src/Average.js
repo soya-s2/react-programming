4b6d760f1948adae2270e2244e8ac4f1177c90bd
soya-s2 <soya.s2.dev@gmail.com>
2021-11-13 02:02:34 +0900

    Use Hooks - useCallback

--- src/Average.js
+++ src/Average.js
@@ -1,6 +1,6 @@
-import React, { useState, useMemo }  from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
-const getAverage = numbers => {
+const getAverage = (numbers) => {
   console.log('평균값 계산 중..');
   if (numbers.length === 0) return 0;
   const sum = numbers.reduce((a, b) => a + b);
@@ -11,15 +11,18 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
 
-  const onChange = e => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  }
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
 
-  const onInsert = e => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber('');
-  };
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber('');
+    },
+    [number, list]
+  ); // number 혹은 list가 바뀌었을 ㄹ때만 함수 생성
 
   const avg = useMemo(() => getAverage(list), [list]);
   /* 
@@ -37,7 +40,7 @@ const Average = () => {
         ))}
       </ul>
       <div>
-        <b>평균값 : </b> {avg} 
+        <b>평균값 : </b> {avg}
       </div>
     </div>
   );
@@ -45,13 +48,16 @@ const Average = () => {
 
 export default Average;
 
-/*
-input 태그에서 onChange가 일어날 때,
-state 값이 변하므로 리렌더링되고,
-계속 평균값을 계산하게 된다. 
-이는 불필요한 연산이다.
+/* 
+useCallback의 첫 번째 파라미터에는 생성하고 싶은 함수를,
+두 번째 파라미터에는 배열을 넣는다.
+이 배열은 어떤 값이 바뀌었을 때 함수를 새로 생성하는지를 명시한다.
 
-useMemo Hook을 사용하면 최적화가 가능하다.
-1. 특정 값이 변할 때만 연산을 실행함
-2. 바뀌지 않았다면 연산 결과를 재사용
+비어 있는 배열을 넣게 되면 컴포넌트 초기 렌더링 시 만들어진 함수를 계속 재사용하고,
+배열에 내용을 넣게 되면 해당 값이 바뀌거나 추가될 때마다 새로 만들어진 함수를 사용한다.
+이것을 '함수 내부에서 상태 값에 의존한다'고 한다.
+
+기존 값을 찾지 않고 설정만 바꾸는 경우는 빈 배열을 넣어도 되지만,
+기존 값을 찾아서 새로운 값을 생성할 때는 꼭 넣어주어야 한다.
+(ex) number와 list를 확인하여 nextList를 만들 때
 */
