a660437b2b3d2cc361c21f0f699c632de38b2da4
soya-s2 <soya.s2.dev@gmail.com>
2021-11-15 15:34:38 +0900

    Use styled-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 const App = () => {
-  return <CSSModule />;
+  return <StyledComponent />;
 };
 
 export default App;
